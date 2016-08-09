ed34e3387b1984517d32fe545d469e9e0004ea8a
Malfolk <sergey.korytko@gmail.com>
2016-08-09 11:56:29 -0400

    PermGen error update

--- module4/src/main/java/com/epam/mentoring/module4/ExampleOutOfMemoryErrorPermGenSpace.java
+++ module4/src/main/java/com/epam/mentoring/module4/ExampleOutOfMemoryErrorPermGenSpace.java
@@ -1,10 +1,20 @@
 package com.epam.mentoring.module4;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 public class ExampleOutOfMemoryErrorPermGenSpace
 {
-    // use jdk 7. set VM options -XX:PermSize=2m and -XX:MaxPermSize=2m.
+    // use jdk 6 :). set VM options -XX:PermSize=10m and -XX:MaxPermSize=10m.
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        List<String> stringList = new LinkedList<String>();
+        Random random = new Random();
+        while (true)
+        {
+            String s = new String(String.valueOf(random.nextFloat())).intern();
+            stringList.add(s);
+        }
     }
 }
